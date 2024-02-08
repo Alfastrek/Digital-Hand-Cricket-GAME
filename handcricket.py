65a7015180adbd20051a6c8a0b63693e04d9a06e
Aradhya Shukla <aradhya1703@gmail.com>
2024-02-08 22:28:36 +0530

    Update handcricket.py

--- handcricket.py
+++ handcricket.py
@@ -1,24 +1,34 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Feb  1 12:52:59 2022
 
-@author: ALFASTREK
-"""
-#HAND CRICKET
+# Introduction
+# Hand cricket is a simple game played between two players or teams. The goal is to score more runs than the opponent while taking turns as batsmen and bowlers.
+
+# Setup
+# Import necessary libraries:
+
 import random
 import pandas as pd
+import numpy as np
+
+# Initialize game parameters:
 
 print('Hello, Welcome to the Hand Cricket Game!')
 gamepl = ['BAT', 'BALL']
 poss = (0, 1, 2, 3, 4, 5, 6)  # Runs Allowed
-userrecord = []  # To maintain individual run record
-comprecord = []
 Players = int(input("Enter Total Players on each side:"))
 User_team = []
 Comp_team = [("ROBO-", i + 1) for i in range(Players)]
+
+
+# Create user team:
+
 for h in range(Players):  # formation of User Team
     names = input("Enter Name of the Players:")
     User_team.append(names)
+
+
+# Overview of the TEAMS
+# Display user and computer teams using pandas DataFrame:
+
 data = {"USER TEAM": User_team,
         "COMP TEAM": Comp_team}
 sr_no = [i + 1 for i in range(Players)]
@@ -26,6 +36,9 @@ df = pd.DataFrame(data, index=[sr_no])
 print("Overview of the TEAMS:", '\n', df)
 
 
+# Define game functions:
+
+# Function for odd-even game
 def oddevengame():
     global oeresult, oddeve
     oddeve = input("\nChoose ODD or EVEN:")
@@ -33,50 +46,51 @@ def oddevengame():
         print("Invalid choice. Choose ODD or EVEN.")
         oddeve = input("Choose ODD or EVEN:")
     oeno = int(input("Choose your no (within 0 to 6):"))  # input for odd-even game
-    rand = random.choice(poss)
+    rand = np.random.choice(poss)
     oeresult = oeno + rand  # total result of odd-even game
     print("Computer Chooses:", rand,
           "\nAnd the Total makes:", oeresult)
 
-
+# Function for batting
 def batting():
-    global userteamtotalruns
+    global userteamtotalruns, userrecord
     ballno = 1  # In order to keep the count for balls
     user_teamno = 0  # In order to display the current player
     userteamtotalruns = 0
-    userscore = 0
+    userrecord = np.array([])
     while user_teamno < Players:
         print("\nBall no:", ballno, "For Player:", User_team[user_teamno])
         runs = int(input('Play Runs:'))
-        rand1 = random.choice(poss)
+        rand1 = np.random.choice(poss)
         print("Computer Chooses:", rand1)
         if runs not in poss:  # extra precaution to maintain fair play
             print("Sorry You are Out!, Only 0 to 6 is the range for runs")
             break
         elif rand1 == runs:
             print(User_team[user_teamno], "is OUT!\n")
-            print("Your Total Runs after batting are:", userscore)
+            print("Your Total Runs after batting are:", userteamtotalruns)
             ballno = 1  # reset the ball serial no.
             user_teamno += 1  # To switch to the next player
-            userteamtotalruns += userscore  # To maintain Team run record
-            userrecord.append(userscore)
+            userteamtotalruns += userrecord.sum()  # To maintain Team run record
             userscore = 0  # To reset the variable for counting runs for the next player
+            userrecord = np.array([])
         else:
-            userscore += runs
+            userscore = np.append(userrecord, runs)
             ballno += 1
             print("Your Runs after this round are:", userscore)
     return "Your Team Total Runs after batting are:", userteamtotalruns
 
-
+# Function for bowling
 def balling():
-    global compteamtotalruns
+    global compteamtotalruns, comprecord
     compscore = 0
     ballno = 1
     comp_teamno = 0
     compteamtotalruns = 0
+    comprecord = np.array([])
     while comp_teamno < Players:
         print("\nBall no:", ballno, "For Player:", Comp_team[comp_teamno])
-        rand2 = random.choice(poss)
+        rand2 = np.random.choice(poss)
         balls = int(input("Enter your Bowling input:"))
         print(Comp_team[comp_teamno], "Chooses:", rand2)
         if balls not in poss:  # extra precaution to maintain fair play
@@ -87,16 +101,16 @@ def balling():
                   "The", Comp_team[comp_teamno], "is OUT!")
             ballno = 1  # reset the ball serial no.
             comp_teamno += 1  # To switch to the next player.
-            compteamtotalruns += compscore  # To maintain Comp team total record
-            comprecord.append(compscore)
+            compteamtotalruns += comprecord.sum()  # To maintain Comp team total record
             compscore = 0
+            comprecord = np.array([])
         else:
-            compscore += rand2
+            compscore = np.append(comprecord, rand2)
             print("Computer Runs after this ball is:", compscore)
             ballno += 1
     return "Computer Team Total Runs are:", compteamtotalruns
 
-
+# Function for final overview
 def final_overview():
     print("\nTOTAL RUNS OF USER TEAM:", userteamtotalruns,
           "\nTOTAL RUNS OF COMPUTER TEAM:", compteamtotalruns)
@@ -114,6 +128,7 @@ def final_overview():
     else:
         print('OK')
 
+# Define the main game function:
 
 def maingame():
     print("\nLet's Play The Classic ODD/EVEN Game First!")
@@ -151,33 +166,6 @@ def maingame():
         else:
             pass
 
+# Run the main game function:
 
 maingame()
-
-                
-        
-        
-    
-        
-        
-
-
-        
-    
-        
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
